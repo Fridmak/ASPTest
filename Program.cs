7402490c659531bbd76a5aeb370152d53ff45e6e
Andrey Batin <androlimp22@gmail.com>
2025-06-26 23:15:58 +0500

    default files usage

--- Program.cs
+++ Program.cs
@@ -2,11 +2,14 @@
 
 var app = builder.Build();
 
-app.MapGet("/sex", ([AsParameters] Person person) => $"You would like to fuck {person.name} who is {person.age} y.o.");
-app.MapGet("/", () => "main");
-
-app.Use(async (context, next) => { await next(); await context.Response.WriteAsync("added"); });
+var options = new DefaultFilesOptions();
+options.DefaultFileNames.Clear();
+options.DefaultFileNames.Add("index.html");
+app.UseDefaultFiles(options);
 app.UseStaticFiles();
+
+app.MapGet("/sex", ([AsParameters] Person person) => $"You would like to fuck {person.name} who is {person.age} y.o.");
+//app.MapGet("/", () => "main");
 
 app.Run();
 
