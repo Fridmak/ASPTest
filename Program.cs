1d1cbf07a4b788aa56b3d04cb781763e05047a0c
Andrey Batin <androlimp22@gmail.com>
2025-07-01 17:44:46 +0500

    to save streak

--- Program.cs
+++ Program.cs
@@ -1,18 +1,36 @@
-﻿using EMPTYASP;
+﻿using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder();
+builder.Configuration.AddJsonFile("person.json");
+builder.Services.Configure<Person>(builder.Configuration);
+
 var app = builder.Build();
 
-builder.Configuration.AddJsonFile("person.json");
-var tom = new Person();
-app.Configuration.Bind(tom);
-
-app.Run(async (context) => await context.Response.WriteAsync($"{tom.Name} - {tom.Age}"));
-
+app.UseMiddleware<PersonMiddleware>();
 app.Run();
+public class PersonMiddleware
+{
+    private readonly RequestDelegate _next;
+    public Person Person { get; }
+    public PersonMiddleware(RequestDelegate next, IOptions<Person> options)
+    {
+        _next = next;
+        Person = options.Value;
+    }
+    public async Task InvokeAsync(HttpContext context)
+    {
+        System.Text.StringBuilder stringBuilder = new();
+        stringBuilder.Append($"<p>Name: {Person.Name}</p>");
+        stringBuilder.Append($"<p>Age: {Person.Age}</p>");
+        stringBuilder.Append("</ul>");
 
+        await context.Response.WriteAsync(stringBuilder.ToString());
+    }
+}
 public class Person
 {
     public string Name { get; set; } = "";
     public int Age { get; set; } = 0;
 }
+
+//52
