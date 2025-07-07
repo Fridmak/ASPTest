9f6e0aaf596c047bee6bca5c3f3167f58f36bced
Andrey Batin <androlimp22@gmail.com>
2025-07-07 09:04:41 +0500

    file logger

---
+++ FileLoggerProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace EMPTYASP
+{
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        string path;
+        public FileLoggerProvider(string path)
+        {
+            this.path = path;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(path);
+        }
+
+        public void Dispose() { }
+    }
+
+    public static class FileLoggerExtensions
+    {
+        public static ILoggingBuilder AddFile(this ILoggingBuilder builder, string filePath)
+        {
+            builder.AddProvider(new FileLoggerProvider(filePath));
+            return builder;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,46 +1,16 @@
-﻿using Microsoft.Extensions.Options;
+﻿using EMPTYASP;
 
 var builder = WebApplication.CreateBuilder();
-builder.Configuration.AddJsonFile("person.json");
-builder.Services.Configure<Person>(builder.Configuration);
-
+// устанавливаем файл для логгирования
+builder.Logging.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "logger.txt"));
+// настройка логгирования с помошью свойства Logging идет до 
+// создания объекта WebApplication
 var app = builder.Build();
-
-app.UseMiddleware<PersonMiddleware>();
 
 app.Run(async (context) =>
 {
-    app.Logger.LogInformation($"Processing request {context.Request.Path}");
-
+    app.Logger.LogInformation($"Path: {context.Request.Path}  Time:{DateTime.Now.ToLongTimeString()}");
     await context.Response.WriteAsync("Hello World!");
 });
 
 app.Run();
-public class PersonMiddleware
-{
-    private readonly RequestDelegate _next;
-    public Person Person { get; }
-    public PersonMiddleware(RequestDelegate next, IOptions<Person> options)
-    {
-        _next = next;
-        Person = options.Value;
-    }
-    public async Task InvokeAsync(HttpContext context)
-    {
-        System.Text.StringBuilder stringBuilder = new();
-        stringBuilder.Append($"<p>Name: {Person.Name}</p>");
-        stringBuilder.Append($"<p>Age: {Person.Age}</p>");
-        stringBuilder.Append("</ul>");
-
-        await context.Response.WriteAsync(stringBuilder.ToString());
-
-        await _next(context);
-    }
-}
-public class Person
-{
-    public string Name { get; set; } = "";
-    public int Age { get; set; } = 0;
-}
-
-//52
